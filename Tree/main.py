ccad9968c8445552718655a54386253537919299
badubidabambirimbum <chetvergovalex@mail.ru>
2024-03-19 14:26:23 +0300

    Layout

--- Tree/main.py
+++ Tree/main.py
@@ -5,6 +5,32 @@ class TreeNode:
         self.right = right
 
 class Tree:
-    def __init__(self):
+    def __init__(self, root=None): # Инициализация
+        self.root = root
         pass
 
+    def __str__(self): # Вывод дерева
+        pass
+
+    def __len__(self): # Вывод длины дерева
+        pass
+
+    def search(self): # Поиск элемента в дереве
+        pass
+
+    def add(self): # Добавление элемента в дерево
+        pass
+
+    def remove(self): # Удаление элемента из дерева
+        pass
+
+    def balancing(self): # Балансировка
+        pass
+
+    def bfs(self):
+        '''Обход дерева в ширину'''
+        pass
+
+    def dfs(self):
+        '''Обход дерева в глубину'''
+        pass
