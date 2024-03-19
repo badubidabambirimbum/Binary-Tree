0e762a41407d75f635d2bdaf31bd48955ac78463
badubidabambirimbum <chetvergovalex@mail.ru>
2024-03-19 13:40:22 +0300

    update 1

--- TreeNode/main.py
+++ TreeNode/main.py
@@ -1,5 +1,11 @@
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right
+
+node = TreeNode(2, TreeNode(1), TreeNode(3))
+
+print(node.val)
+print(node.left.val)
+print(node.right.val)
